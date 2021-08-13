dad13e0a17188cee1445da620123114a79ce8285
Nina Weiler <ninaweiler3@gmail.com>
2021-08-13 10:01:12 +0300

    Added global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import ImageGridView from './pages/ImageGridView';
 import DetailsView from './pages/DetailsView'
 import GalleryView from './pages/GalleryView';
 import NavBar from './components/NavBar';
+import GlobalStyles from './styles/GlobalStyle';
 
 function App() {
   return (
-    <div className='App'>
+    <div >
       <BrowserRouter>
+      <GlobalStyles />
       <NavBar />
       <Switch>
         <Route exact path='/' component={GalleryView} />
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,44 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+}
+
+html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+
+    @media only screen and (max-width: 1200px){
+        font-size: 58%;
+    }
+    @media only screen and (min-width: 1980px){
+        font-size: 70%;
+    }
+}
+
+body{
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    font-weight: 400;
+    line-height: 1.6;
+    font-size: 1.6rem;
+    background: #FAFAFA;
+    color: black;
+    text-align: center;
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    
+
+}
+`;
+
+export default GlobalStyles;
